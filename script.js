248e2febcdd5087ea3914ae6309786c1b21789bf
Ester B. Oliveira <esterworkoliveira@outlook.com>
2023-02-01 12:59:18 -0300

    teste

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
          loop:true
      });
      var typed = new Typed(".typing-2", {
-        strings:["YouTuber", "Desenvolvedor", "Designer", "Freelancer"],
+        strings:["Desenvolvedora", "Analista", "Designer", "Freelancer"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
